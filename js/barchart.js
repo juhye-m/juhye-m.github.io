482e8c969b26dd523e73e3fe41969ec2c35c26dc
Juliet Chihaya <julietchihaya@dhcp-10-250-4-161.harvard.edu>
2022-11-13 23:14:29 -0500

    added line chart & bar chart

---
+++ js/barchart.js
@@ -0,0 +1,31 @@
+class BarChart {
+
+    constructor(parentElement, data) {
+        this.parentElement = parentElement;
+        this.data
+
+        this.initVis()
+    }
+
+    initVis() {
+        let vis = this;
+
+        vis.margin = {top: 20, right: 20, bottom: 20, left: 40};
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
+        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
+
+        // init drawing area
+        vis.svg = d3.select("#" + vis.parentElement).append("svg")
+            .attr("width", vis.width + vis.margin.left + vis.margin.right)
+            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+            .append('g')
+            .attr('transform', `translate (${vis.margin.left}, ${vis.margin.top})`);
+
+        this.wrangleData();
+    }
+
+    wrangleData() {
+        let vis = this;
+    }
+
+}
--- js/main.js
+++ js/main.js
@@ -1,27 +1,58 @@
 
 // skeleton stuff - left comments on places that need to be worked on
+let linegraph;
 
 // need to parse date string and convert to time
-// let parseDate = d3.timeParse("%Y"); 
+let parseDate = d3.timeParse("%Y");
+
+// if data is top year 2021 or 2020
+// then time parse week, parseDateWeek = d3.timeParse() , and then get the year
 
 // Load CSV file - we can change this too? to be more like the recent psets?
 function loadData() {
-	d3.csv("data/OUR_DATA_FILENAME.csv", row => {
-        // placeholder, we need to convert our strings to numbers
+	d3.csv("data/fixed_data.csv", row => {
+		row.Acousticness = +row.Acousticness
+		row.BPM = +row.BPM
+		row.Danceability = +row.Danceability
+		row.Duration_ms = +(row.Duration_ms/1000)
+		row.Energy = +row.Energy
+		row.Liveness = +row.Liveness
+		row.Loudness_db = +row.Loudness_db
+		row.Popularity = +row.Popularity
+		row.Speechiness = +row.Speechiness
+		row.Valence = +row.Valence
+		//row.ReleaseDate = +row.ReleaseDate
+		row.TopYear = +row.TopYear
+
+
 		// row.year = parseDate(row.year);
 
 		return row
 	}).then(data => {
         // we should make classes for our visualizations
-        // linechart_characteristics = new LineChart("line-chart-area",data)
+		console.log(data)
+		let count = 0
+		data.forEach(function(d,i) {
+			if (data[i].TopYear == 2021) {
+
+				count = count + 1
+			}
+		})
+		console.log(count)
+
+		linegraph = new LineChart("line-chart-area",data)
+		bargraph = new BarChart("chart1",data)
 	});
 }
 
+loadData()
 
 // store category selection - for dropdown
 let selectedCategory =  document.getElementById('categorySelector').value;
+console.log(selectedCategory)
 
 function categoryChange() {
    selectedCategory =  document.getElementById('categorySelector').value;
 //    linechart_characteristics.wrangleData();
 }
+
