23d51dbabcc9a6fdf7c287a2ecc84919dedd7706
Juliet Chihaya <julietchihaya@dhcp-10-250-4-161.harvard.edu>
2022-11-21 00:44:04 -0500

    fix spacing

--- js/barchart.js
+++ js/barchart.js
@@ -11,11 +11,10 @@ class BarChart {
         let vis = this;
 
         vis.margin = {top: 40, right: 20, bottom: 20, left: 40};
-        //vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
-        //vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
-
-        vis.width = 500
-        vis.height = 500
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
+        vis.height = 600 - vis.margin.top - vis.margin.bottom;
+        //vis.width = 500
+        //vis.height = 500
 
         // init drawing area
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
--- js/linechart.js
+++ js/linechart.js
@@ -17,11 +17,10 @@ class LineChart {
     initVis() {
         let vis = this;
         vis.margin = {top: 40, right: 40, bottom: 50, left: 50};
-        //vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
-        //vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
-
-        vis.width = 500
-        vis.height = 500
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
+        vis.height = 600 - vis.margin.top - vis.margin.bottom;
+        //vis.width = 500
+        //vis.height = 500
 
         // init drawing area
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
